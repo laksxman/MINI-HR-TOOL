2eb2160c054ddc03fc4468363250f48a64967a35
Lakshman Kumar <kumarlaksxman17@gmail.com>
2026-04-10 14:09:13 +0530

    backend url added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': 'http://localhost:5000'
+      proxy: {
+      '/api': {
+        target: 'https://mini-hr-tool-backend-wurz.onrender.com',
+        changeOrigin: true,
+        secure: false
+      }
     }
   },
   resolve: {
